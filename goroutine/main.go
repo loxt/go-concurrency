4807337105bc8995dfc8870f3a298158a255d193
emannuel <emannuelloxt@hotmail.com>
2023-10-29 10:18:00 -0400

    feat: add wait group

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	go printSomething("One")
-	printSomething("Two")
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go printSomething("One", &wg)
+	go printSomething("Two", &wg)
+
+	wg.Wait()
 }
 
-func printSomething(value string) {
-	for {
+func printSomething(value string, wg *sync.WaitGroup) {
+	for i := 0; i < 5; i++ {
 		fmt.Println(value)
 		time.Sleep(time.Millisecond * 500)
 	}
+
+	defer wg.Done()
 }
